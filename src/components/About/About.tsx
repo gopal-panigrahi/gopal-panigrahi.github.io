361b2e56e99e6975f350ff2414974100ef51e7d5
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2023-11-23 22:38:43 +0530

    adding content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,16 +5,12 @@ function About() {
     <div id="about" className="flex items-center border-t border-black py-4">
       <h1 className="w-1/4 text-center text-2xl">About Me</h1>
       <p className="flex-1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in
-        iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla
-        commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi
-        dapibus porta quam laoreet placerat. Donec eu vehicula neque. Donec
-        viverra lorem nunc, tempus euismod eros sollicitudin ut. Quisque et
-        tincidunt libero. Donec id pharetra justo. Proin euismod lacinia dolor,
-        eu scelerisque justo tempus pharetra. Vivamus nunc justo, finibus ut
-        nisl sed, euismod rhoncus nulla. Proin sed magna egestas, egestas ante
-        et, congue eros. In consequat, mauris dapibus tincidunt suscipit, ex
-        libero aliquet diam, at maximus risus enim non leo.
+        Experienced software engineer with a strong background in frontend
+        development using React.JS and backend development with C# ASP.NET.
+        Proficient in creating user-friendly web applications, collaborating
+        with cross-functional teams, and keeping up with emerging technologies.
+        Skilled in problem-solving and committed to delivering efficient,
+        maintainable code.
       </p>
     </div>
   );
--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -8,43 +8,75 @@ function Experience() {
     >
       <h1 className="w-1/4 text-center text-2xl">Experience</h1>
       <div className="flex-1 py-4">
-        <div className="mb-4 w-11/12 rounded-lg border p-3">
-          <h3 className="text-xl font-medium">Persistent Systems</h3>
-          <h5 className="text-lg">Senior Software Engineer</h5>
-          <p className="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sed
-            laborum id recusandae quibusdam sunt quis magni cumque nemo saepe
-            nisi, corrupti repellendus voluptates praesentium nostrum? Facilis
-            aspernatur quisquam porro earum repellendus praesentium ducimus
-            vitae esse consequuntur facere consequatur placeat voluptate
-            exercitationem quam assumenda dicta temporibus, modi ipsam itaque.
-            Eveniet!
+        <div className="mb-4 w-11/12 rounded-lg border p-4">
+          <div className="flex flex-row justify-between">
+            <h3 className="text-xl font-medium">Persistent Systems</h3>
+            <h5 className="text-sm">Jul 2023 - Present</h5>
+          </div>
+          <h5 className="text-md">Senior Software Engineer</h5>
+          <p className="pl-4 text-sm">
+            <ul className="list-disc">
+              <li>
+                Worked upon creating a portal for gathering real-time data from
+                kidney dialysis IOT devices.
+              </li>
+              <li>
+                Developed user-friendly frontend for real-time visualization
+                using charts.
+              </li>
+              <li>
+                Worked to optimize performance of charts for handling enormous
+                amount of data on browser.
+              </li>
+              <li>
+                Implemented a tailor-made caching mechanism on UI using chartjs
+                and redux.
+              </li>
+              <li>
+                Worked on setting up the unit testing framework for project.
+              </li>
+              <li>Got awarded with Top Talent Award 2023</li>
+            </ul>
           </p>
         </div>
-        <div className="mb-4 w-11/12 rounded-lg border p-3">
-          <h3 className="text-xl font-medium">Persistent Systems</h3>
-          <h5 className="text-lg">Software Engineer</h5>
-          <p className="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sed
-            laborum id recusandae quibusdam sunt quis magni cumque nemo saepe
-            nisi, corrupti repellendus voluptates praesentium nostrum? Facilis
-            aspernatur quisquam porro earum repellendus praesentium ducimus
-            vitae esse consequuntur facere consequatur placeat voluptate
-            exercitationem quam assumenda dicta temporibus, modi ipsam itaque.
-            Eveniet!
+        <div className="mb-4 w-11/12 rounded-lg border p-4">
+          <div className="flex flex-row justify-between">
+            <h3 className="text-xl font-medium">Persistent Systems</h3>
+            <h5 className="text-sm">Jul 2021 - Jul 2023</h5>
+          </div>
+          <h5 className="text-md">Software Engineer</h5>
+          <p className="pl-4 text-sm">
+            <ul className="list-disc">
+              <li>
+                Worked upon Patient and Clinician which is a Digital Healthcare
+                Platform for Kidney dialysis patients and clinicians.
+              </li>
+              <li>
+                Worked upon Smart Healthcare Device Integration with browser
+                platform for receiving real-time vitals.
+              </li>
+              <li>Worked with Umbraco and C# for handling backend logic.</li>
+            </ul>
           </p>
         </div>
-        <div className="mb-4 w-11/12 rounded-lg border p-3">
-          <h3 className="text-xl font-medium">Persistent Systems</h3>
-          <h5 className="text-lg">Software Development Intern</h5>
-          <p className="text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo sed
-            laborum id recusandae quibusdam sunt quis magni cumque nemo saepe
-            nisi, corrupti repellendus voluptates praesentium nostrum? Facilis
-            aspernatur quisquam porro earum repellendus praesentium ducimus
-            vitae esse consequuntur facere consequatur placeat voluptate
-            exercitationem quam assumenda dicta temporibus, modi ipsam itaque.
-            Eveniet!
+        <div className="mb-4 w-11/12 rounded-lg border p-4">
+          <div className="flex flex-row justify-between">
+            <h3 className="text-xl font-medium">Persistent Systems</h3>
+            <h5 className="text-sm">Jan 2021 - Jul 2021</h5>
+          </div>
+          <h5 className="text-md">Software Development Intern</h5>
+          <p className="pl-4 text-sm">
+            <ul className="list-disc">
+              <li>
+                Worked upon a internal project for Learning Portal, which
+                tracked progress of course for each learner.
+              </li>
+              <li>
+                Worked upon Post-Course certificate generation after successful
+                course completion using @react-pdf
+              </li>
+              <li>Got awarded with GEMS certificate.</li>
+            </ul>
           </p>
         </div>
       </div>
