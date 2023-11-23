73da2af0a9e9f59644efe03d4426853aac4a69a1
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2023-11-23 23:07:18 +0530

    added deploy scripts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,12 +3,13 @@ import type {GatsbyConfig} from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Profile`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://gopal-panigrahi.github.io`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  pathPrefix: "/gopal-panigrahi.github.io",
   plugins: ["gatsby-plugin-postcss"]
 };
 
